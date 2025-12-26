826e2a0ecb136f706f508916ddb892f77fa6e707
shloktech <shloktadilkar@gmail.com>
2025-12-27 00:18:56 +0530

    Configuring Unit Tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
 import pytest
 
 from src.keyedstablehash.canonical import canonicalize_to_bytes
